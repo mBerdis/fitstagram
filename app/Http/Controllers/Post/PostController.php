cb6f4b95f29655fba03524e25c010617a2d00a47
Maroš Berdis <marosberdis@gmail.com>
2024-10-21 21:29:46 +0200

    feat: service init

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Post;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Services\PostRetrievalService;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
 use App\Models\User;
 use Inertia\Inertia;
@@ -11,12 +13,12 @@ use Inertia\Response;
 
 class PostController extends Controller
 {
-    public function create(Request $request): Response
+    public function create(Request $request, PostRetrievalService $postService): Response
     {
-        $post = Post::with('owner', 'comments', 'comments.user')->get();
+        $posts = $postService->get_personal_feed();
 
         return Inertia::render('Feed', [
-            'posts' => $post
+            'posts' => $posts
         ]);
     }
 }
---
+++ app/Services/PostRetrievalService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+
+class PostRetrievalService
+{
+    // returns posts to show at /feed page
+    public function get_personal_feed()
+    {
+        $user = Auth::user();
+
+        return Post::with('owner', 'comments', 'comments.user') // include comments and comments author
+        //->where('is_public', '==', true)    // get public posts
+        //->orWhere()                         // get posts from friends
+        //->orWhere()                         // get posts from a group which user is a member of
+        ->orderBy('created_at')
+        ->get();
+    }
+
+    // returns posts to show at /profile page
+    public function get_user_images($user_id)
+    {
+        if (PostRetrievalService::has_access($user_id))
+        {
+            // show public and private posts since logged user has access to them
+            return Post::with('owner', 'comments', 'comments.user') // include comments and comments author
+            ->whereHas('owner', fn($query) => $query->where('user_id', $user_id))
+            ->orderBy('created_at')
+            ->get();
+        }
+
+        return Post::with('owner', 'comments', 'comments.user') // include comments and comments author
+        ->whereHas('owner', fn($query) => $query->where('user_id', $user_id))
+        ->where('is_public', true)    // get public posts
+        ->orderBy('created_at')
+        ->get();
+    }
+
+    // returns posts to show at /profile page
+    public function get_group_images($group_id)
+    {
+        // TODO:
+        return Post::with('owner', 'comments', 'comments.user')-where()->get();
+    }
+
+    private function has_access($user_id): bool
+    {
+        $loggedUser = Auth::user();
+
+        if ($loggedUser->id == $user_id)
+        {
+            return true;
+        }
+
+        // TODO: check if he is a friend
+    }
+}
