49ce8a2aa548c3765cb72229babcab7910c8cfb9
Koscelnik <matej.koscelnik@gmail.com>
2024-12-13 18:55:36 +0100

    feat: Post feel

--- resources/js/Components/Post/Post.vue
+++ resources/js/Components/Post/Post.vue
@@ -168,136 +168,138 @@ const removePostFromGroup = () => {
 </script>
 
 <template>
-    <div class="post-container bg-white dark:bg-gray-800">
-     <div class="relative inline-block group">
-       <!-- Post Image -->
-       <img
-         :src="post.photo"
-         alt="Post Photo"
-         class="post-photo rounded-lg cursor-pointer"
-         @click="expandImage(post.photo)"
-       />
+    <div class="post-container ">
+      <div class="relative  group">
+        <!-- Post Image -->
+        <img
+          :src="post.photo"
+          alt="Post Photo"
+          class="post-photo rounded-lg cursor-pointer"
+          @click="expandImage(post.photo)"
+        />
 
-       <div
-         class="absolute top-2 left-2 bg-black bg-opacity-50 p-2 rounded-lg flex items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-       >
-         <UserListView :user="post.owner" />
-       </div>
-       <!-- Like Button (Bottom-Right Corner) -->
-       <div class="absolute bottom-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-           <Like :post="post" />
-         </div>
-     </div>
+        <div
+          class="absolute top-2 left-2 bg-white bg-opacity-50 p-2 rounded-lg flex items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+        >
+          <UserListView :user="post.owner" />
+        </div>
+        <!-- Like Button (Bottom-Right Corner) -->
+        <div class="absolute bottom-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <Like :post="post" />
+        </div>
+      </div>
 
-     <div v-if="isImageExpanded" class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50" @click="closeOverlay">
-       <div class="relative w-11/12 max-w-7xl bg-white dark:bg-gray-800 p-4 rounded-lg shadow-lg" @click.stop>
-         <button class="absolute top-2 right-3 text-white text-3xl" @click="closeOverlay">&times;</button>
+      <div v-if="isImageExpanded" class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50" @click="closeOverlay">
+        <div class="relative w-11/12 max-w-7xl bg-white dark:bg-gray-800 p-4 rounded-lg shadow-lg" @click.stop>
+          <button class="absolute top-2 right-3 text-white text-3xl" @click="closeOverlay">&times;</button>
 
-         <!-- Roll Menu -->
-         <div class="absolute px-0 py-0">
-           <button
-               v-if="canDelete() || canRemoveFromGroup() || canEdit()"
-               class="p-2 rounded-full transition-opacity duration-300 transition-colors duration-300"
-               :class="{
-               'bg-transparent': !isRollMenuOpen,
-               'bg-gray-700 text-white': isRollMenuOpen
-               }"
-               @click="toggleRollMenu"
-           >
-               <svg v-if="!isRollMenuOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-               </svg>
-               <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-               </svg>
-           </button>
+          <!-- Roll Menu -->
+          <div class="absolute px-0 py-0">
+            <button
+              v-if="canDelete() || canRemoveFromGroup() || canEdit()"
+              class="p-2 rounded-full transition-opacity duration-300 transition-colors duration-300"
+              :class="{
+                'bg-transparent': !isRollMenuOpen,
+                'bg-gray-700 text-white': isRollMenuOpen
+              }"
+              @click="toggleRollMenu"
+            >
+              <svg v-if="!isRollMenuOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
 
-           <div
-               v-if="isRollMenuOpen"
-               class="absolute mt-0 left-0 bg-gray-100 bg-opacity-90 dark:bg-gray-700 dark:bg-opacity-90 shadow-md rounded-md py-2 w-48 z-50"
-           >
-               <button
-                   v-if="canDelete()"
-                   class="block w-full text-left px-4 py-2 hover:bg-red-100 dark:hover:bg-red-500 dark:hover:text-white"
-                   @click="deletePost"
-               >
-                   Delete
-               </button>
-               <button
-                   v-if="canRemoveFromGroup()"
-                   class="block w-full text-left px-4 py-2 hover:bg-red-100 dark:hover:bg-red-500 dark:hover:text-white"
-                   @click="removePostFromGroup"
-               >
-                   Remove from Group
-               </button>
-               <button
-                   v-if="canEdit()"
-                   class="block w-full text-left px-4 py-2 hover:bg-blue-100 dark:hover:bg-blue-500 dark:hover:text-white"
-                   @click="togglePrivacy"
-               >
-                   {{ post.is_public ? 'Make Private' : 'Make Public' }}
-               </button>
-               <button
-                   v-if="canEdit()"
-                   class="block w-full text-left px-4 py-2 hover:bg-yellow-100 dark:hover:bg-yellow-500 dark:hover:text-white"
-                   @click="enableDescriptionEdit"
-               >
-                   Edit Description
-               </button>
-           </div>
-         </div>
+            <div
+              v-if="isRollMenuOpen"
+              class="absolute mt-0 left-0 bg-gray-100 bg-opacity-90 dark:bg-gray-700 dark:bg-opacity-90 shadow-md rounded-md py-2 w-48 z-50"
+            >
+              <button
+                v-if="canDelete()"
+                class="block w-full text-left px-4 py-2 hover:bg-red-100 dark:hover:bg-red-500 dark:hover:text-white"
+                @click="deletePost"
+              >
+                Delete
+              </button>
+              <button
+                v-if="canRemoveFromGroup()"
+                class="block w-full text-left px-4 py-2 hover:bg-red-100 dark:hover:bg-red-500 dark:hover:text-white"
+                @click="removePostFromGroup"
+              >
+                Remove from Group
+              </button>
+              <button
+                v-if="canEdit()"
+                class="block w-full text-left px-4 py-2 hover:bg-blue-100 dark:hover:bg-blue-500 dark:hover:text-white"
+                @click="togglePrivacy"
+              >
+                {{ post.is_public ? 'Make Private' : 'Make Public' }}
+              </button>
+              <button
+                v-if="canEdit()"
+                class="block w-full text-left px-4 py-2 hover:bg-yellow-100 dark:hover:bg-yellow-500 dark:hover:text-white"
+                @click="enableDescriptionEdit"
+              >
+                Edit Description
+              </button>
+            </div>
+          </div>
 
-         <div class="flex flex-col md:flex-row">
-           <!-- Image section -->
-           <div class="w-full md:w-1/2">
-             <img :src="selectedImage" alt="Expanded Post Photo" class="w-full rounded-lg" />
-           </div>
+          <div class="flex flex-col md:flex-row">
+  <!-- Image section -->
+  <div class="w-full md:w-3/5">
+    <img :src="selectedImage" alt="Expanded Post Photo" class="h-auto max-h-full rounded-lg w-full object-fill" />
+  </div>
 
-           <!-- Description -->
-           <div class="w-full md:w-1/2 md:ml-4 mt-4 md:mt-0">
-             <div class="image-details mt-4">
-               <UserListView :user="post.owner" />
+  <!-- Description -->
+  <div class="w-2/5 md:ml-4 mt-4 md:mt-0">
+    <div class="image-details mt-4">
+      <UserListView :user="post.owner" />
 
-               <p v-if="!isEditMenuOpen" class="text-gray-700 dark:text-gray-300 mt-2">{{ post.description }}</p>
-               <div v-if="isEditMenuOpen" class="flex items-center space-x-2 mt-4">
-                 <TextInput
-                   id="editableDescription"
-                   type="text"
-                   class="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
-                   v-model="form.content"
-                   required
-                   autofocus
-                 />
-                 <button
-                   @click="editDescription"
-                   class="px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:hover:bg-blue-600"
-                   :disabled="form.processing"
-                 >
-                   Save
-                 </button>
-               </div>
+      <p v-if="!isEditMenuOpen" class="text-gray-700 dark:text-gray-300 mt-2">{{ post.description }}</p>
+      <div v-if="isEditMenuOpen" class="flex items-center space-x-2 mt-4">
+        <TextInput
+          id="editableDescription"
+          type="text"
+          class="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+          v-model="form.content"
+          required
+          autofocus
+        />
+        <button
+          @click="editDescription"
+          class="px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:hover:bg-blue-600"
+          :disabled="form.processing"
+        >
+          Save
+        </button>
+      </div>
 
-               <div class="scroll-container">
-                 <AddTag v-if="canDelete()" :post_id="post.id" ></AddTag>
-                 <Tag v-for="tag in post.tags" :tag="tag" :can_delete="canDelete()" />
-               </div>
+      <div class="scroll-container">
+        <AddTag v-if="canDelete()" :post_id="post.id" ></AddTag>
+        <Tag v-for="tag in post.tags" :tag="tag" :can_delete="canDelete()" />
+      </div>
 
-               <p class="text-gray-500 dark:text-gray-400 text-sm">{{ getPostAge }}</p>
-               <div class="absolute top-4 right-4 flex items-center space-x-2">
-                   <p class="text-gray-700 dark:text-gray-300 mt-2">{{ post.like_count }}</p>
-                   <Like :post="post" />
-               </div>
-             </div>
-             <!-- Comments section -->
-             <div class="comments-section">
-               <CommentsSection :post="post" />
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
+      <p class="text-gray-500 dark:text-gray-400 text-sm">{{ getPostAge }}</p>
+      <div class="absolute top-4 right-4 flex items-center space-x-2">
+        <p class="text-gray-700 dark:text-gray-300 mt-2">{{ post.like_count }}</p>
+        <Like :post="post" />
+      </div>
     </div>
-   </template>
+    <!-- Comments section -->
+    <div class="comments-section">
+      <CommentsSection :post="post" />
+    </div>
+  </div>
+</div>
+
+        </div>
+      </div>
+    </div>
+  </template>
+
 
    <style scoped>
    .scroll-container {
@@ -346,8 +348,8 @@ const removePostFromGroup = () => {
    }
 
    .fixed .relative {
-     width: 90%;
-     max-width: 1024px;
+     width: 95%;
+     max-width: 90hw;
      padding: 1rem;
    }
 
